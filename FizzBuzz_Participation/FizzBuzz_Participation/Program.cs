d3e70762506960021bfe26a449a34a66156c1292
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-09-08 12:19:32 -0500

    Completed Fizz Buzz participation

---
+++ FizzBuzz_Participation/FizzBuzz_Participation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz_Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            //if the number has a remainder = number % 3; aka remainder of 3
+            //bool hasRemainder = remainder == 0;
+
+            //BOOLEAN OPERATORS:
+            //      OR  = ||
+            //      AND = &&
+            number = 15;
+            if ((number % 3 == 0) && (number % 5 == 0))
+                
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (number % 3 == 0)
+
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+
+            {
+                Console.WriteLine("Buzz");
+            }
+            else Console.WriteLine(number);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
