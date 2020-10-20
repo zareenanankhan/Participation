3707725ea9a3c5ba8f1440d57bea40d996e75ca5
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-10-20 12:10:23 -0500

    COMMIT

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 4; i > 0; i--)
+            {
+                Console.WriteLine("There are " + i + " puppies left.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animaltype;
+            string animalsound;
+
+            Console.WriteLine("Please enter a type of animal");
+            animaltype = Console.ReadLine();
+
+            animalsound=Speak(animaltype);
+
+            Console.WriteLine(animalsound);
+
+            Console.ReadKey();
+
+        }
+
+
+        static string Speak(string animaltype)
+            {
+           
+            string animalsound;
+
+            if (animaltype == "dog")
+            {
+                animalsound = "bark";
+                
+            }
+            else if (animaltype == "monkey")
+            {
+                animalsound = "chatter";
+               
+            }
+            else if (animaltype == "goat")
+            {
+                animalsound = "bleat";
+
+            }
+            else
+            {
+                animalsound = "Animal not found";
+            }
+
+            return animalsound;
+
+            Console.ReadKey();
+
+            }
+        
+    }
+}
