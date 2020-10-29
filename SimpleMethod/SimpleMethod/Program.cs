a650cfd6ec3a9d7df72544b4b75811402c8a5414
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-10-29 12:31:24 -0500

    COMMIT

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
