66878b6e78f2b2abfee1e62ed3b9a55938fd510c
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-11-10 12:30:06 -0600

    Completed

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle = new Circle();
+            myCircle.Radius = 7.5;
+            Console.WriteLine($"The area of a circle with {myCircle.Radius} is {myCircle.CalculateArea()}\n" +
+                              $"   and a perimeter of {myCircle.CalculatePerimeter()}");
+
+            Circle circle2 = new Circle();
+            circle2.Radius = 8282828282;
+            double areaOfCircle2 = circle2.CalculateArea();
+            double perimeterOfCircle2 = circle2.CalculatePerimeter();
+
+            Circle circle3 = new Circle(3.5);
+
+            Rectangle myRectangle = new Rectangle();
+            myRectangle.length = 2;
+            myRectangle.width = 2;
+            Console.WriteLine($"The area of the rectangle with {myRectangle.length} and {myRectangle.width} is {myRectangle.CalculateAreaRect()}\n" +
+                $"and the perimeter is {myRectangle.CalculatePerimeterRect()}");
+
+            Rectangle rectangle2 = new Rectangle();
+            rectangle2.length = 2;
+            rectangle2.width = 2;
+            double areaofrect2 = rectangle2.CalculateAreaRect();
+            double perimeterofrect2 = rectangle2.CalculatePerimeterRect();
+
+            Rectangle rect3 = new Rectangle(4);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes/Classes/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Classes
+{
+    class Rectangle
+    {
+        private int v;
+
+        public double length { get; set; }
+        public double width { get; set; }
+
+        public Rectangle()
+        {
+            length = 0;
+            width = 0;
+        }
+
+        public Rectangle(int v)
+        {
+            this.v = v;
+        }
+
+        public double CalculateAreaRect()
+        {
+            return length * width;
+        }
+        
+        public double CalculatePerimeterRect()
+        {
+            return length + length + width + width;
+        }
+    }
+}
