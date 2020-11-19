ea5821f99b8c455e944f65c25f69114a880079c0
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2020-11-19 12:26:56 -0600

    COMMIT

---
+++ ClassesTextBox/ClassesTextBox/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesTextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Textbox MyTextbox = new Textbox();
+            MyTextbox.Padding = 50;
+            MyTextbox.Text = "I NEED SLEEP";
+            MyTextbox.BackColor = ConsoleColor.White;
+            MyTextbox.ForeColor = ConsoleColor.Black;
+
+            MyTextbox.DisplayText();
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ ClassesTextBox/ClassesTextBox/Textbox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesTextBox
+{
+    class Textbox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+
+        public Textbox()
+        {
+            Padding = 50;
+            BackColor = ConsoleColor.White;
+            ForeColor = ConsoleColor.Black;
+            Text = "";
+
+        }
+
+        public void DisplayText()
+        {
+            //int pad = 50;
+            //Text = "insert random text here";
+            Text = Text.PadLeft(Text.Length+Padding, ' ');
+            Text = Text.PadRight(Text.Length+Padding, ' ');
+
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            Console.WriteLine(Text);
+
+        }
+    }
+
+}
